cb808c442b1869016e5b03faabebef5968c98474
MarianaDraganescu <draganescu.mariana@yahoo.com>
2022-04-16 22:38:30 +0300

    first commit

---
+++ ManagementTool/src/test/java/ro/mariana/managementtool/ManagementToolApplicationTests.java
@@ -0,0 +1,13 @@
+package ro.mariana.managementtool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagementToolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

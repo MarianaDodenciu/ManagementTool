3a5bd989beeac5378b9217e7dda7ed53c25627ed
MarianaDraganescu <draganescu.mariana@yahoo.com>
2022-04-18 21:18:40 +0300

    Added attributes to Project model and repository

--- ManagementTool/src/main/java/ro/mariana/domain/Project.java
+++ ManagementTool/src/main/java/ro/mariana/domain/Project.java
@@ -1,4 +1,183 @@
 package ro.mariana.domain;
 
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
 public class Project {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String projectName;
+    private String projectIdentifier;
+    private String description;
+    private Date startDate;
+    private Date endDate;
+
+    private Date createdAt;
+    private Date updatedAt;
+
+    public Project() {
+    }
+
+    @PrePersist
+    private void onCreate() {
+        this.createdAt = new Date();
+    }
+
+    @PreUpdate
+    private void onUpdate() {
+        this.updatedAt = new Date();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getProjectName() {
+        return projectName;
+    }
+
+    public void setProjectName(String projectName) {
+        this.projectName = projectName;
+    }
+
+    public String getProjectIdentifier() {
+        return projectIdentifier;
+    }
+
+    public void setProjectIdentifier(String projectIdentifier) {
+        this.projectIdentifier = projectIdentifier;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Date getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Date updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+
+    public static final class ProjectBuilder {
+        private Long id;
+        private String projectName;
+        private String projectIdentifier;
+        private String description;
+        private Date startDate;
+        private Date endDate;
+        private Date createdAt;
+        private Date updatedAt;
+
+        private ProjectBuilder() {
+        }
+
+        public static ProjectBuilder aProject() {
+            return new ProjectBuilder();
+        }
+
+        public ProjectBuilder withId(Long id) {
+            this.id = id;
+            return this;
+        }
+
+        public ProjectBuilder withProjectName(String projectName) {
+            this.projectName = projectName;
+            return this;
+        }
+
+        public ProjectBuilder withProjectIdentifier(String projectIdentifier) {
+            this.projectIdentifier = projectIdentifier;
+            return this;
+        }
+
+        public ProjectBuilder withDescription(String description) {
+            this.description = description;
+            return this;
+        }
+
+        public ProjectBuilder withStartDate(Date startDate) {
+            this.startDate = startDate;
+            return this;
+        }
+
+        public ProjectBuilder withEndDate(Date endDate) {
+            this.endDate = endDate;
+            return this;
+        }
+
+        public ProjectBuilder withCreatedAt(Date createdAt) {
+            this.createdAt = createdAt;
+            return this;
+        }
+
+        public ProjectBuilder withUpdatedAt(Date updatedAt) {
+            this.updatedAt = updatedAt;
+            return this;
+        }
+
+        public Project build() {
+            Project project = new Project();
+            project.setId(id);
+            project.setProjectName(projectName);
+            project.setProjectIdentifier(projectIdentifier);
+            project.setDescription(description);
+            project.setStartDate(startDate);
+            project.setEndDate(endDate);
+            project.setCreatedAt(createdAt);
+            project.setUpdatedAt(updatedAt);
+            return project;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Project{" +
+                "id=" + id +
+                ", projectName='" + projectName + '\'' +
+                ", projectIdentifier='" + projectIdentifier + '\'' +
+                ", description='" + description + '\'' +
+                ", startDate=" + startDate +
+                ", endDate=" + endDate +
+                ", createdAt=" + createdAt +
+                ", updatedAt=" + updatedAt +
+                '}';
+    }
 }
---
+++ ManagementTool/src/main/java/ro/mariana/repositories/ProjectRepository.java
@@ -0,0 +1,12 @@
+package ro.mariana.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import ro.mariana.domain.Project;
+
+@Repository
+public interface ProjectRepository extends CrudRepository<Project, Long> {
+
+    @Override
+    Iterable<Project> findAllById(Iterable<Long> longs);
+}

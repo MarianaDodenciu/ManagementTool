aa5d8aee4627bc09b1e80879d7f6407d69eed1de
MarianaDraganescu <draganescu.mariana@yahoo.com>
2022-04-26 21:59:07 +0300

    Added ProjectItem and Header components

--- management-tool-react-client/src/App.js
+++ management-tool-react-client/src/App.js
@@ -1,11 +1,13 @@
 import { Component } from 'react';
 import './App.css';
 import Dashboard from './components/Dashboard';
+import Header from './components/Layout/Header';
 
 class App extends Component {
   render() {
     return (
       <div className="App-header">
+        <Header />
         <Dashboard />
       </div>
     );
--- management-tool-react-client/src/components/Dashboard.js
+++ management-tool-react-client/src/components/Dashboard.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import ProjectItem from './Project/ProjectItem';
 
 class Dashboard extends Component {
     render() {
         return (
             <div>
-                <h1>Welcome to the DashBoard</h1>
+                <ProjectItem />
             </div>
         );
     }

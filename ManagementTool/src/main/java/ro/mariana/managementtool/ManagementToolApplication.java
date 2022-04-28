909efbc13191e5720b54a5c952b45ea4b866930c
MarianaDraganescu <draganescu.mariana@yahoo.com>
2022-04-28 22:01:48 +0300

    Created connection to mySQL database and removed flyway dependency due to error

--- ManagementTool/src/main/java/ro/mariana/managementtool/ManagementToolApplication.java
+++ ManagementTool/src/main/java/ro/mariana/managementtool/ManagementToolApplication.java
@@ -2,8 +2,10 @@ package ro.mariana.managementtool;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan(basePackages = {"ro.mariana.domain"}) //needed to create automatically the tables
 public class ManagementToolApplication {
 
     public static void main(String[] args) {

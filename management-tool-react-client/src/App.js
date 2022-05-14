d5ea978d70a93943831a3b97030e6d2c4e77cc21
MarianaDraganescu <draganescu.mariana@yahoo.com>
2022-05-14 21:53:41 +0300

    Added registration/login/landing page html

--- management-tool-react-client/src/App.js
+++ management-tool-react-client/src/App.js
@@ -4,6 +4,9 @@ import Dashboard from './components/Dashboard';
 import Header from './components/Layout/Header';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import AddProject from './components/Project/AddProject';
+import Landing from './components/Layout/Landing';
+import Login from './components/UserManagement/Login';
+import Register from './components/UserManagement/Register';
 
 class App extends Component {
   render() {
@@ -11,6 +14,17 @@ class App extends Component {
       <Router>
         <div className="App-header">
           <Header />
+          {
+            //Public Routes
+          }
+
+          <Route exact path="/" component={Landing} />
+          <Route exact path="/register" component={Register} />
+          <Route exact path="/login" component={Login} />
+
+          {
+            //Private Routes
+          }
           <Route exact path="/dashboard" component={Dashboard} />
           <Route exact path="/addProject" component={AddProject} />
         </div>
